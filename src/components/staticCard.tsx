c4296e880ed52d6653b1ac61702bd32fee3ed272
sreekanth666 <srekthk@gmail.com>
2024-07-14 00:33:53 +0530

    feat: Add StaticCard component

---
+++ src/components/staticCard.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react';
+
+interface StaticCardProps {
+    title: string;
+    description: string;
+}
+
+const StaticCard: FC<StaticCardProps> = ({ title, description }) => {
+    return (
+        <div className='font-roboto flex flex-col items-center mt-14 md:mt-20'>
+            <p className='font-extralight text-5xl md:text-6xl'>
+                {title}
+            </p>
+            <p className='font-light w-[47%] md:w-[67%] leading-tight text-lg mt-4'>
+                {description}
+            </p>
+        </div>
+    );
+};
+
+export default StaticCard;
