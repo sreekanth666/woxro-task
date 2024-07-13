1e1c9f10f2dbc93c2b407a3471b7aed45146f6fa
sreekanth666 <srekthk@gmail.com>
2024-07-14 00:34:02 +0530

    feat: Refactor Card component to use dynamic image and description props

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -2,20 +2,22 @@ import Image from 'next/image';
 import React, { FC } from 'react';
 
 interface CardProps {
-
+    image: string;
+    alt: string;
+    description: string;
 }
 
-const Card: FC<CardProps> = ({ }) => {
+const Card: FC<CardProps> = ({ image, alt, description }) => {
     return (
         <div>
             <Image
-                src='/images/card/screenshot-1.webp'
-                alt='Epic Games Fortnite game image'
+                src={image}
+                alt={alt}
                 width={800}
                 height={800}
             />
             <p className='text-center font-roboto font-light leading-tight mt-4 md:mt-6'>
-                Explore large, destructible environments where no two games are ever the same.
+                {description}
             </p>
         </div>
     );
